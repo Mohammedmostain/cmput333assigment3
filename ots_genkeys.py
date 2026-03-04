3899a4d87e5aa9ecfff056febc32f88bd5ae7a68
sowale8 <jamestuason8@gmail.com>
2026-03-04 11:13:08 -0700

    removed unnecessary code

--- ots_genkeys.py
+++ ots_genkeys.py
@@ -21,22 +21,7 @@ def hashSecretKeyBlock(block: bytes) -> bytes:
     takes a 256-bit block (32-bytes) in hex, and applies SHA3-256 hash function 16 times
     returns the hashed block    
     '''
-    # todo: isnt this supposed to be 16 times?
-    # result = subprocess.run(
-    #     [
-    #         "openssl", "dgst", "-sha3-256", "-binary"
-    #     ],
-    #     input=block,
-    #     stdout=subprocess.PIPE,
-    #     stderr=subprocess.PIPE,
-    # )
-    # return result.stdout
     return hashlib.sha3_256(block).digest()
-
-# def extractHashedBlock(output:bytes):
-#     outputStr = output.decode()
-#     hashedBlock = outputStr.split("=")[1].strip()
-#     return hashedBlock
 
 def main():
     sk_arr = []
@@ -49,11 +34,6 @@ def main():
         sk_arr.append(sk_i)
         pk_arr.append(cur)
     
-    # block = generateSecretKeyBlock()
-    # sk = block
-    # pk = hashSecretKeyBlock(block)
-    # print(sk)
-    # print(pk)
     Path("private_key.ots").write_bytes(b"".join(sk_arr))
     Path("public_key.ots").write_bytes(b"".join(pk_arr))
 
